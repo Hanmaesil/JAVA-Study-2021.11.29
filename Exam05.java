25b70adeb57f07a7eb9b56f0edfa8c5bc7a3cba2
Hanmaesil <93114691+Hanmaesil@users.noreply.github.com>
2021-11-29 17:47:49 +0900

    method의 리턴 타입

---
+++ Exam05.java
@@ -0,0 +1,26 @@
+
+public class Exam05 {
+
+	public static void main(String[] args) {
+
+		// 메소드의 리턴타입.
+		// 1. int, String, float, double...... : 리턴 타입이 존재 할 때.
+		// 2. void : 리턴 타입이 존재하지 않을 때.
+
+		Hello("빅데이터 반");
+
+	}
+
+	public static void Hello(String str) {
+		
+		
+		System.out.println(str + " 반갑습니다~"); //메소드 안에서 출력 가능.
+		
+	}
+	
+	
+	
+	
+	
+	
+}
